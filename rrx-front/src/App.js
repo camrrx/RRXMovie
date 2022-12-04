c86da6f4ea5b90867ece78d9127e20989dacf295
Camille Roux <Camille@MacBook-Pro-2.local>
2022-12-04 21:32:04 +0100

    reparer les conneries de Thibaut

--- rrx-front/src/App.js
+++ rrx-front/src/App.js
@@ -7,11 +7,8 @@ import Register from "./components/register/register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux";
-import { useSelector } from "react-redux";
 
 function App() {
-	const loginUserData = useSelector((state) => state.loginUser);
-
 	return (
 		<React.Fragment>
 			<Provider store={store}>
